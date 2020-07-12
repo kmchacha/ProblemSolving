fe862462af44e6357b19430493c1e301df09c69c
rudals0215 <63946027+rudals0215@users.noreply.github.com>
2020-07-13 00:44:43 +0900

    pps 문제풀이

---
+++ CPP연습/pps2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+    int a, b, sum=0;
+
+    cin >> a >> b;
+
+    for(int i=a;i<=b;i++){
+        if(i==b) {
+            sum += i;
+            cout << i << "=" << sum;
+            break;
+        }
+        cout << i << "+";
+        sum += i;
+    }
+
+    return 0;
+}
