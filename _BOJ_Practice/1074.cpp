0b33c964845eb6932b433718046ee736ac57de76
rudals0215 <>
2021-02-04 03:05:02 +0900

    recursion
    재귀 연습

---
+++ _BOJ_Practice/1074.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// 2^n x 2^n 배열에서 (r,c)를 방문하는 순서를 반환하는 함수
+int func(int n, int r, int c){
+    if(n==0) return 0;
+    else{
+        int half = 1 << (n-1);
+        if(r < half &&  c < half) return func(n-1, r, c);
+        if(r < half && c >= half) return half*half + func(n-1, r, c-half);
+        if(r >= half && c < half) return 2*half*half + func(n-1,r-half,c);
+        return 3*half*half + func(n-1, r-half, c-half);
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int n, r, c;
+    cin >> n;
+    cin >> r >> c;
+    cout << func(n, r, c);
+    return 0;
+}
